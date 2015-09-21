fb1960bbb9a687dfea3ee924e3ef566661322630
maoyan <zhouqimaoyan@163.com>
2015-09-21 21:02:32 +0800

    9.21

--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ window.onload = function(){
 		}
 	});
 
-
 //alarm 框
 	var alarm_box = $(".alarm");
 	var alarm_close = alarm_box.find('b');
@@ -47,27 +46,41 @@ window.onload = function(){
 		hide_alarm();
 	});
 
+//clear
+	var clear_overlay = $(".clear");
+	clear_overlay.click(function(event) {
+		map.clearOverlays();
+		map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
+	});
 
 //注记marker
- var icon_url = "http://webmap0.map.bdstatic.com/wolfman/static/common/images/us_cursor_9517a2b.png";
+ var icon_url = "http://webmap2.map.bdstatic.com/wolfman/static/common/images/spotmkrs_ad2672a.png";
  var bike_icon_url = "http://webmap1.map.bdstatic.com/wolfman/static/common/images/ui3/mo_banner_ba37b5d.png";
- var icon_blue = new BMap.Icon(icon_url, new BMap.Size(18,26), {"imageOffset":new BMap.Size(-18,-22)});
- var icon_red = new BMap.Icon(icon_url, new BMap.Size(18,26), {"imageOffset":new BMap.Size(-36,-22)});
+ var icon_sub = new BMap.Icon(icon_url, new BMap.Size(22,22), {"imageOffset":new BMap.Size(-35,-25)});
+ var icon_exc = new BMap.Icon(icon_url, new BMap.Size(26,28), {"imageOffset":new BMap.Size(-138,-25)});
  var icon_bike = new BMap.Icon(bike_icon_url, new BMap.Size(25,25), {"imageOffset":new BMap.Size(-105,-215)});
-//1号线站点
-	var line1 = $("#ss_menu").find('div:nth-child(1)').find('h1');
-	line1.click(function(event) {
+ var icon_trail = new BMap.Icon(icon_url, new BMap.Size(22,22), {"imageOffset":new BMap.Size(-252,-202)});
+   //1号线站点
+    var line1 = $("#ss_menu").find('div:nth-child(1)').find('h1');
+		line1.click(function(event) {
 		//覆盖点
 		//map.clearOverlays();
 		var point = [],marker = [],label=[];
-			 for (var i = 0; i < 25; i++) {
+			 for (var i = 0; i < 31; i++) {
 			 	point.push(new BMap.Point(line1_point[i].lon,line1_point[i].lat));
-			 	marker.push(new BMap.Marker(point[i],{icon:icon_red}));  // 创建标注
+			 	if(i == 5||i == 9||i==12||i==18||i==24||i==29){
+			 		marker.push(new BMap.Marker(point[i],{icon:icon_exc}));  // 创建标注
+			 	}else{
+			 		marker.push(new BMap.Marker(point[i],{icon:icon_sub}));
+			 	}
+			 		
 			 	map.addOverlay(marker[i]);               // 将标注添加到地图中
-			 	label[i] = new BMap.Label(line1_point[i].name,{"offset":new BMap.Size(10,20)});//站名主注记
+			 	label[i] = new BMap.Label(line1_point[i].name,{"offset":new BMap.Size(10,-20)});//站名主注记
 	            marker[i].setLabel(label[i]);
-	            map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
+	            //map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
 			 };
+			  	var polyline = new BMap.Polyline(point, {strokeColor:"#f00", strokeWeight:6, strokeOpacity:0.5});  //定义折线
+				map.addOverlay(polyline);     //添加折线到地图上  
 	});
 
 	//2号线站点
@@ -76,32 +89,81 @@ window.onload = function(){
 			//覆盖点
 		// map.clearOverlays();
 		var point = [],marker = [],label=[];
-		for (var i = 0; i < 16; i++) {
+		for (var i = 0; i < 24; i++) {
 		 	point.push(new BMap.Point(line2_point[i].lon,line2_point[i].lat));
-		 	marker.push(new BMap.Marker(point[i],{icon:icon_blue}));  // 创建标注
-		 	map.addOverlay(marker[i]);               // 将标注添加到地图中
-		 	label[i] = new BMap.Label(line2_point[i].name,{"offset":new BMap.Size(10,20)});//站名主注记
-	        marker[i].setLabel(label[i]);
-	        map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
-		 };
-	});
+		 	if(i == 3||i == 5||i == 8||i == 11||i == 17||i == 21){
+			 		marker.push(new BMap.Marker(point[i],{icon:icon_exc}));  // 创建标注
+			 	}else{
+			 		marker.push(new BMap.Marker(point[i],{icon:icon_sub}));
+			 	}
 
+		 	map.addOverlay(marker[i]);               // 将标注添加到地图中
+		 	label[i] = new BMap.Label(line2_point[i].name,{"offset":new BMap.Size(10,-20)});//站名主注记
+	        marker[i].setLabel(label[i]);
+	       // map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
+		 };
+		  var polyline = new BMap.Polyline(point, {strokeColor:"#00f", strokeWeight:6, strokeOpacity:0.5});  //定义折线
+		  map.addOverlay(polyline);     //添加折线到地图上
+	});
 
 	//3号线站点
 	var line3 = $("#ss_menu").find('div:nth-child(3)').find('h1');
 		line3.click(function(event) {
 		//覆盖点
-		//map.clearOverlays();
-		show_alarm();
+			var point = [],marker = [],label=[];
+			for (var i = 0; i < 22; i++) {
+			 	point.push(new BMap.Point(line3_point[i].pos.split(",")[0],line3_point[i].pos.split(",")[1]));
+			 	if(i == 2||i == 4||i==11||i==13||i==18||i==21){
+				 		marker.push(new BMap.Marker(point[i],{icon:icon_exc}));  // 创建标注
+				 	}else{
+				 		marker.push(new BMap.Marker(point[i],{icon:icon_sub}));
+				 	}
+			 	map.addOverlay(marker[i]);               // 将标注添加到地图中
+			 	label[i] = new BMap.Label(line3_point[i].name,{"offset":new BMap.Size(10,-20)});//站名主注记
+		        marker[i].setLabel(label[i]);
+		        //map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
+			 };
+			   var polyline = new BMap.Polyline(point, {strokeColor:"#802a2a", strokeWeight:6, strokeOpacity:0.5});  //定义折线
+			   map.addOverlay(polyline);     //添加折线到地图上
 		});
 
 	//4号线站点
 	var line4 = $("#ss_menu").find('div:nth-child(4)').find('h1');
 		line4.click(function(event) {
 		//覆盖点
-		//map.clearOverlays();
-		show_alarm();
+			var point = [],marker = [],label=[];
+			for (var i = 0; i < 16; i++) {
+			 	point.push(new BMap.Point(line4_point[i].pos.split(",")[0],line4_point[i].pos.split(",")[1]));
+			 	if(i == 1||i == 5||i == 9||i==13){
+				 		marker.push(new BMap.Marker(point[i],{icon:icon_exc}));  // 创建标注
+				 	}else{
+				 		marker.push(new BMap.Marker(point[i],{icon:icon_sub}));
+				 	}
+			 	map.addOverlay(marker[i]);               // 将标注添加到地图中
+			 	label[i] = new BMap.Label(line4_point[i].name,{"offset":new BMap.Size(10,-20)});//站名主注记
+		        marker[i].setLabel(label[i]);
+		        //map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
+			 };
+			   var polyline = new BMap.Polyline(point, {strokeColor:"#0f0", strokeWeight:6, strokeOpacity:0.5});  //定义折线
+			   map.addOverlay(polyline);     //添加折线到地图上
 		});
+
+//铁路
+	var trailway = $(".trail");
+	trailway.click(function(event) {
+		var point = [],marker = [],label=[];
+			for (var i = 0; i < 14; i++) {
+			 	point.push(new BMap.Point(trail_point[i].pos.split(",")[0],trail_point[i].pos.split(",")[1]));
+				marker.push(new BMap.Marker(point[i],{icon:icon_trail}));
+			 	map.addOverlay(marker[i]);               // 将标注添加到地图中
+			 	label[i] = new BMap.Label(trail_point[i].name,{"offset":new BMap.Size(10,-20)});//站名主注记
+		        marker[i].setLabel(label[i]);
+		        //map.centerAndZoom(new BMap.Point(120.898576,31.984992), 13);
+			 };
+			   var polyline = new BMap.Polyline(point, {strokeColor:"#000", strokeWeight:6, strokeOpacity:0.5});  //定义折线
+			   map.addOverlay(polyline);     //添加折线到地图上
+	});
+
 
 
 //左侧拉出栏
@@ -140,8 +202,8 @@ window.onload = function(){
 	var bike_search = $('#search > i');
 	var side_list = $('#side_bar > ul > li:nth-child(2) > div >ul');
 	bike_search.click(function(event) {
-		 map.clearOverlays();
-		 side_list.html('');
+		 //map.clearOverlays();
+		 side_list.empty();
 		 var keyword = $('#search > input').val();//输入的关键字
 		     var point = [],marker = [],label=[];
 		if(!keyword){
